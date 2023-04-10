0ba4823f8a8e04a433d053ee4c56244fa5639b17
kauankarvalho <kauan.karvalho@outlook.com>
2023-04-09 21:58:19 -0300

    feat: add focus() to search input

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ const sidebar = document.querySelector(".js-sidebar")
 const menu = document.querySelector(".js-menu")
 const items = document.querySelectorAll(".js-item")
 
-menu.addEventListener("click", () => {
+function openAndCloseSidebar() {
   body.classList.toggle("c-body--sidebar-close")
   sidebar.classList.toggle("c-sidebar--close")
 
   items.forEach((item) => {
     item.classList.toggle("is-hidden")
   })
+}
+
+menu.addEventListener("click", openAndCloseSidebar)
+
+const searchIcon = document.querySelector(".js-search-icon")
+const searchInput = document.querySelector(".c-sidebar__search")
+
+searchIcon.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.contains("is-hidden") ? openAndCloseSidebar() : ""
+  })
+  searchInput.focus()
 })
