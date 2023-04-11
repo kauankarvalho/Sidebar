324ddfaa6e37f5e9b39bd5e8650c45b5c93b20b7
kauankarvalho <kauan.karvalho@outlook.com>
2023-04-10 21:34:23 -0300

    feat: add responsive menu functionality for mobile devices

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
-const body = document.querySelector("body")
-const sidebar = document.querySelector(".js-sidebar")
-const menu = document.querySelector(".js-menu")
-const items = document.querySelectorAll(".js-item")
+function detectScreenSize() {
+  const mediaQuery = window.matchMedia("(min-width: 690px)")
 
-function openAndCloseSidebar() {
-  body.classList.toggle("c-body--sidebar-close")
-  sidebar.classList.toggle("c-sidebar--close")
+  const menu = document.querySelector(".js-menu")
+  const searchIcon = document.querySelector(".js-search-icon")
+  const searchInput = document.querySelector(".c-sidebar__search")
 
-  items.forEach((item) => {
-    item.classList.toggle("is-hidden")
-  })
+  if (mediaQuery.matches) {
+    const body = document.querySelector("body")
+    const sidebar = document.querySelector(".js-sidebar")
+    const items = document.querySelectorAll(".js-item")
+
+    function openAndCloseSidebarDesktop() {
+      body.classList.toggle("c-body--sidebar-close")
+      sidebar.classList.toggle("c-sidebar--close")
+
+      items.forEach((item) => {
+        item.classList.toggle("is-hidden")
+      })
+    }
+
+    menu.addEventListener("click", openAndCloseSidebarDesktop)
+    searchIcon.addEventListener("click", () => {
+      items.forEach((item) => {
+        item.classList.contains("is-hidden") ? openAndCloseSidebarDesktop() : ""
+      })
+      searchInput.focus()
+    })
+  } else {
+    const list = document.querySelector(".js-list")
+    list.classList.add("is-hidden")
+
+    function openAndCloseMenuMobile() {
+      list.classList.toggle("is-hidden")
+    }
+
+    menu.addEventListener("click", openAndCloseMenuMobile)
+    searchIcon.addEventListener("click", () => searchInput.focus())
+  }
 }
 
-menu.addEventListener("click", openAndCloseSidebar)
-
-const searchIcon = document.querySelector(".js-search-icon")
-const searchInput = document.querySelector(".c-sidebar__search")
-
-searchIcon.addEventListener("click", () => {
-  items.forEach((item) => {
-    item.classList.contains("is-hidden") ? openAndCloseSidebar() : ""
-  })
-  searchInput.focus()
-})
+detectScreenSize()
